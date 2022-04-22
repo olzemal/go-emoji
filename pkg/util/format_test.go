ff2088f7a0cba97dbfb6899b3115d603009ff37d
Alexander Olzem <olzemal@pm.me>
2022-04-22 12:54:50 +0200

    Add tests for util

---
+++ pkg/util/format_test.go
@@ -0,0 +1,28 @@
+package util_test
+
+import (
+	"testing"
+
+	"github.com/olzemal/lsemoji/pkg/util"
+)
+
+func TestFormat(t *testing.T) {
+	tests := []struct {
+		I string
+		O string
+	}{
+		{I: "hello", O: "hello"},
+		{I: "hello world", O: "hello_world"},
+		{I: "hello & world", O: "hello_world"},
+		{I: "a (world)", O: "a_world"},
+		{I: "hello     world", O: "hello_world"},
+		{I: "Hello World", O: "hello_world"},
+	}
+
+	for _, test := range tests {
+		got := util.Format(test.I)
+		if got != test.O {
+			t.Errorf("Got: %v, Want: %v", got, test.O)
+		}
+	}
+}
---
+++ pkg/util/path_test.go
@@ -0,0 +1,51 @@
+package util_test
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/olzemal/lsemoji/pkg/util"
+)
+
+const (
+	cacheFileName = "cache"
+)
+
+func TestSetup(t *testing.T) {
+	tmpDirPath := SetupTestTmp()
+	defer CleanUp(tmpDirPath)
+	if !util.CanCreate(tmpDirPath + "/" + cacheFileName) {
+		t.Errorf("TestSetup: cannot create `%s`", tmpDirPath+"/"+cacheFileName)
+	}
+}
+
+func TestReadFrom(t *testing.T) {
+	tmpDirPath := SetupTestTmp()
+	defer CleanUp(tmpDirPath)
+	f, err := os.Create(tmpDirPath + "/" + cacheFileName)
+	if err != nil {
+		t.Errorf("TestReadFrom: cannot create `%s`", tmpDirPath+"/"+cacheFileName)
+	}
+	defer f.Close()
+	if !util.CanReadFrom(tmpDirPath + "/" + cacheFileName) {
+		t.Errorf("TestReadFrom: cannot read from `%s`", tmpDirPath+"/"+cacheFileName)
+	}
+}
+
+func SetupTestTmp() string {
+	path, err := os.MkdirTemp("/tmp", "lsemoji_util_test_")
+	if err != nil {
+		panic(fmt.Errorf("SetupTestTemp: %v", err))
+	}
+	log.Println("Created: ", path)
+	return path
+}
+
+func CleanUp(tmpDir string) {
+	if err := os.RemoveAll(tmpDir); err != nil {
+		panic(fmt.Errorf("CleanUp: %v", err))
+	}
+	log.Println("Removed: ", tmpDir)
+}
