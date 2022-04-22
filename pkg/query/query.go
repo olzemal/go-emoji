cf9e2529aa8ac1a0fc9e91040fff80f60357d820
Alexander Olzem <olzemal@pm.me>
2022-04-22 12:06:29 +0200

    Filter with Contains instead of regex

--- pkg/query/query.go
+++ pkg/query/query.go
@@ -1,17 +1,14 @@
 package query
 
 import (
-	"regexp"
-
-	"github.com/olzemal/lsemoji/pkg/cache"
+	"strings"
 )
 
-func Search(query string, c cache.Cache) []string {
-	var results []string
-	substr, _ := regexp.Compile(".*" + query + ".*")
-	for k := range c {
-		if substr.MatchString(k) {
-			results = append(results, c[k])
+func FilterContains(m map[string]string, s string) map[string]string {
+	results := make(map[string]string)
+	for k, v := range m {
+		if strings.Contains(k, s) {
+			results[k] = v
 		}
 	}
 	return results
